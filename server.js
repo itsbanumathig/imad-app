4514deabc35259b9d1b252da327e893d0976c030
itsbanumathig <31466040+itsbanumathig@users.noreply.github.com>
2017-09-02 16:10:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-two', function (req, res) {
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article threee will be available here');
+  res.send('Article three will be available here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
